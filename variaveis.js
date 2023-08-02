51740bf8b58ac4a0408b1f413dc79d01e11a9f83
giovannadso <127310419+giovannadso@users.noreply.github.com>
2023-08-02 10:40:08 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,93 @@
+// Questão 01 //
+
+var favoriteFood = 'pizza';
+
+// Questão 02 //
+
+var numOfSlices = 8;
+
+// Questão 03 //
+
+console.log(favoriteFood)
+
+// Questão 04 //
+
+console.log(numOfSlices)
+
+// Questão 05 //
+
+/*Diferença entre "var" e "let":
+A principal diferença entre "var" e "let" está no escopo das variáveis. Quando uma variável é declarada com "var", ela tem escopo de função ou escopo global, dependendo de onde foi declarada. Isso significa que a variável pode ser acessada em qualquer lugar dentro da função ou globalmente se estiver fora de uma função.
+Por outro lado, quando uma variável é declarada com "let", ela tem escopo de bloco. Isso significa que a variável só pode ser acessada dentro do bloco onde foi declarada, como dentro de um loop, condicional ou qualquer outro bloco delimitado por chaves "{}".*/
+
+// Questão 06 //
+
+let changeMe = true;
+
+// Questão 07 //
+
+changeMe = false;
+
+// Questão 08 //
+
+console.log(changeMe)
+
+// Questão 09 //
+
+const entree = 'Enchiladas'
+
+// Questão 10 //
+
+console.log(entree)
+
+// Questão 11 //
+
+entree = 'Burrito'; // Isso deve gerar um erro, pois "entree" é uma variável constante e não pode ser reatribuída.
+
+// Questão 12 //
+
+let levelUp = 10;
+levelUp += 5; 
+console.log(levelUp)
+
+// Questão 13 //
+
+let powerLevel = 10;
+powerLevel -= 100; 
+console.log(powerLevel)
+
+// Questão 14 //
+
+let multiplyMe = 10;
+multiplyMe *= 11; 
+console.log(multiplyMe)
+
+// Questão 15 //
+
+let quarterMe = 10;
+quarterMe /= 4; 
+console.log(quarterMe)
+
+// Questão 16 //
+
+let gainedDollar = 1;
+gainedDollar++;
+console.log(gainedDollar); 
+
+// Questão 17 //
+
+let lostDollar = 5;
+lostDollar--;
+console.log(lostDollar); 
+
+// Questão 18 //
+
+let myName = 'Giovanna';
+let myCity = 'New York';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+// Questão 19 //
+
+let newVariable = 42;
+console.log(typeof newVariable); 
+
